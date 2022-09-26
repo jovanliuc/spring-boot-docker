0898b1c2fca3ade25983dc83f345552b19ea2fe7
Jovan L <327527689@qq.com>
2022-09-26 23:27:34 +0800

    big integer number plus

---
+++ src/test/java/datastructure/bigintegernumberaddition/BigIntegerNumberAddition.java
@@ -0,0 +1,9 @@
+package datastructure.bigintegernumberaddition;
+
+public class BigIntegerNumberAddition {
+
+    public static void main(String[] args) {
+        int result = Integer.MAX_VALUE + 1;
+        System.out.println(result);
+    }
+}
