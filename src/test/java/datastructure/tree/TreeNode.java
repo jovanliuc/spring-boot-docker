8f53b7ecf49b7b96549b91313bd181b2bea386e7
Jovan L <327527689@qq.com>
2022-09-29 16:19:26 +0800

    added binary search tree data structure

---
+++ src/test/java/datastructure/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package datastructure.tree;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
