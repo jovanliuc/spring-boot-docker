5b91befdf09db5d75221470a428e05df70e4ca05
Jovan L <327527689@qq.com>
2022-10-04 23:23:03 +0800

    added the first solution of leetcode 0027

---
+++ src/test/java/leetcode/question0027/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.question0027;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+
+    }
+
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+
+        return slow;
+    }
+}
