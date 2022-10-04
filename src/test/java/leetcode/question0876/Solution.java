e3040f1c00030a3f5e4f52415d9ea938c219f25d
Jovan L <327527689@qq.com>
2022-10-04 18:11:48 +0800

    added first solution of leetcode 0876

---
+++ src/test/java/leetcode/question0876/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.question0876;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        middleNode(head);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
