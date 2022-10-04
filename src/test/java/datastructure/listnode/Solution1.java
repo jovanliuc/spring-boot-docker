85c635a2e97daa988dbd7e455c148c91f226e8bd
Jovan L <327527689@qq.com>
2022-10-04 18:51:22 +0800

    added the first solution of leetcode 0141

---
+++ src/test/java/datastructure/listnode/Solution1.java
@@ -0,0 +1,36 @@
+package datastructure.listnode;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution1 {
+
+    @Test
+    void test() {
+        hasCycle(null);
+    }
+
+    public ListNode hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}
---
+++ src/test/java/leetcode/question0141/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.question0141;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution {
+
+    @Test
+    void test() {
+        hasCycle(null);
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
