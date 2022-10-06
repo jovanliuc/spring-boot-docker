14b2c53fcd65a8373bdfb1ca9dfbcc6c47dad20e
Jovan L <327527689@qq.com>
2022-10-06 18:16:30 +0800

    added the second solution of leetcode 0112

---
+++ src/test/java/leetcode/question0112/Solution1.java
@@ -0,0 +1,30 @@
+package leetcode.question0112;
+
+import org.junit.jupiter.api.Test;
+
+public class Solution1 {
+
+    @Test
+    void test() {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(20);
+        root.right = new TreeNode(30);
+        root.left.left = new TreeNode(40);
+        root.left.right = new TreeNode(50);
+        root.right.left = new TreeNode(60);
+        root.right.right = new TreeNode(80);
+
+        hasPathSum(root, 100);
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == targetSum) {
+            return true;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+}
