3e9c64510cd0496c34f9e54e3c5ed86f791447de
Jovan L <327527689@qq.com>
2022-10-06 12:42:23 +0800

    added the first solution of leetcode 0102

---
+++ src/test/java/leetcode/question0102/Solution.java
@@ -0,0 +1,50 @@
+package leetcode.question0102;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+
+    @Test
+    void test() {
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(20);
+        root.right = new TreeNode(30);
+        root.left.left = new TreeNode(40);
+        root.left.right =  new TreeNode(50);
+        root.right.left = new TreeNode(60);
+        root.right.right = new TreeNode(80);
+        levelOrder(root);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+                if (current.left != null) {
+                    queue.add(current.left);
+                }
+                if (current.right != null) {
+                    queue.add(current.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
