8229b742a13540922dab269e1b9684449d1b3589
Jovan L <327527689@qq.com>
2022-10-06 22:17:44 +0800

    modified the solution of leetcode 0206

--- src/test/java/leetcode/question0206/Solution.java
+++ src/test/java/leetcode/question0206/Solution.java
@@ -6,7 +6,12 @@ public class Solution {
 
     @Test
     void test() {
-        reverseList(null);
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        reverseList(head);
     }
 
     public ListNode reverseList(ListNode head) {
